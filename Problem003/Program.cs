1c2fb9c2c2436304e647f765eaef4dcfc567628c
Aaron Portis <aaronjportis@gmail.com>
2025-08-02 18:57:48 +0100

    Problem 3

---
+++ Problem003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ans - 6857
+
+long num = 600851475143;
+
+int currentDivisor = 3;
+
+do{
+ if(num % currentDivisor == 0) num = num/currentDivisor;
+ else currentDivisor = currentDivisor += 2;
+}while(num != 1);
+
+Console.Write(currentDivisor);
+Console.Read();
