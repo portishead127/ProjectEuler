85648992f0253353c9dbb887bbcb8f5faa838eaf
Aaron Portis <aaronjportis@gmail.com>
2025-08-02 18:12:52 +0100

    Problem 2 -kinda badly

--- Problem002/Program.cs
+++ Problem002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int sum = 2;
+bool numExceeded = false;
+Fibonnaci(1,2);
+
+Console.Write(sum);
+Console.Read();
+
+void Fibonnaci(int first, int second){
+  int tempSum = first + second;
+  if(tempSum > 4000000 || numExceeded){
+    numExceeded = true;
+    return;
+  }
+  if(tempSum % 2 == 0) sum += tempSum;
+  Fibonnaci(second, tempSum);
+}
