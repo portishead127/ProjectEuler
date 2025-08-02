ab01ed6f45e73ffd6b7f402aff3dce450601be5b
Aaron Portis <aaronjportis@gmail.com>
2025-08-02 16:56:14 +0100

    some nonsense

--- Problem1/obj/Debug/net9.0/Problem1.AssemblyInfo.cs
+++ Problem1/obj/Debug/net9.0/Problem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1ddf9d93a62774038190e756fdb70c92c72158")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
