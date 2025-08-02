b4842ac894585021266fd1b732d477209d4d6bb8
Aaron Portis <aaronjportis@gmail.com>
2025-08-02 18:13:21 +0100

    Random project files being weird

--- Problem001/obj/Debug/net9.0/Problem001.AssemblyInfo.cs
+++ Problem001/obj/Debug/net9.0/Problem001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d04befccc3feedf2a10e8285dc8e361d24d39c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59bf4e3ba13b8440f7157eb489c24ed76c60a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
