07ae276913ce2663786f2cda91d7d11fa0f8539f
Aaron Portis <aaronjportis@gmail.com>
2025-08-07 17:04:29 +0100

    Problem 4

--- Problem004/Program.cs
+++ Problem004/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//There is absolutely no chance this is the fastest way to find this
+//Ans - 906609
+
+int maxPalindrome = -1;
+
+for(int i = 100; i < 1000; i++){
+  for(int j = 100; j < 1000; j++){
+    int prod = i * j;
+    if(isPalindromic(prod) && prod > maxPalindrome) maxPalindrome = prod;
+  }
+}
+
+Console.Write(maxPalindrome);
+
+bool isPalindromic(int num){
+  char[] digits = num.ToString().ToCharArray();
+  bool palindromic = true;
+  for(int i = 0; i < digits.Length/2; i++){
+    if(digits[i] != digits[digits.Length - 1 - i]) palindromic = false;
+  }
+  return palindromic;
+}
