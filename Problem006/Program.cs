9d1703166fbf5947784f02986264549ba5127e79
Aaron Portis <aaronjportis@gmail.com>
2025-09-01 14:19:49 +0100

    Problem006

--- Problem006/Program.cs
+++ Problem006/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int target = 100;
+
+int SumOfSquares(){
+  int total = 0;
+  for(int i = 1; i <= target; i++){
+    total += i *i;
+  }
+  return total;
+} 
+
+int SquareOfSum(){
+  int total = 0;
+  for(int i = 1; i <= target; i++){
+    total += i;
+  }
+  return total * total;
+}
+
+Console.WriteLine("Sum of squares: " + SumOfSquares());
+Console.WriteLine("Square of sum: " + SquareOfSum());
+
