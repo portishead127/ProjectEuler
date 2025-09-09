f9ae9db08f0e195a6e5c9ae80e2bd4aa49187c79
Aaron Portis <aaronjportis@gmail.com>
2025-09-09 14:28:56 +0100

    Problem 009

---
+++ Problem009/Program.cs
@@ -0,0 +1,28 @@
+﻿//Finding pythag triplets
+//a^2 + b^2 = c^2
+//Some triplet where a + b + c = 1000
+int maxC = 1000;
+
+for(double a = 1; a < maxC; a++){
+  for(double b = 1; b < maxC; b++){
+    double cSquared = a*a + b*b;
+    if(isPerfectSquare(cSquared)){
+      if(CheckEligibility(a,b,Math.Sqrt(cSquared))) return;
+    }
+  }
+}
+
+bool isPerfectSquare(double num){
+  double root = Math.Sqrt(num);
+  if(Math.Round(root) == root) return true;
+  return false;
+}
+
+bool CheckEligibility(double a, double b, double c){
+  if(a + b + c == 1000) {
+    Console.WriteLine(a + "+" + b + "+" + c + "=" + 1000);
+    return true;
+  }
+  return false;
+}
+
