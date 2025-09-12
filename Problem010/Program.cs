66a3bca6c9dab8b6d5ec7c3d6c9f30ac6a5f6dae
Aaron Portis <aaronjportis@gmail.com>
2025-09-12 12:28:57 +0100

    Problem010

--- Problem010/Program.cs
+++ Problem010/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Ans - 142913828922
+
+int target = 2000000;
+
+long sum = 0;
+for(long i = 2; i < target; i++){
+  if(IsPrime(i)) sum += i;
+}
+
+Console.WriteLine(sum);
+
+bool IsPrime(long num){
+  double sqrNum = Math.Floor(Math.Sqrt(num));
+ 
+  for (int i = 2; i <= sqrNum; i++){
+    if(num % i == 0) return false;
+  }
+  return true;
+}
