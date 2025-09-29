0320279735248a81b347e9ae6e04578a56afafa3
Aaron Portis <aaronjportis@gmail.com>
2025-09-29 09:33:30 +0100

    Added an even worse way to do this problem. Great.

---
+++ Problem012/Program.cs
@@ -0,0 +1,14 @@
+﻿int targetDivisors = 500;
+long currentTriangleNum = 1;
+while(numOfDivisors(currentTriangleNum) <= targetDivisors) currentTriangleNum = currentTriangleNum + currentTriangleNum + 1;
+Console.WriteLine(currentTriangleNum);
+
+int numOfDivisors(long n){
+  int divisors = 0;
+  int count = 1;
+  while(count < n){
+    if(n % count == 0) divisors++;
+    count++;
+  }
+  return divisors;
+}
