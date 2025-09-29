3f299277da4d835622c367486a871bb40c74707a
Aaron Portis <aaronjportis@gmail.com>
2025-09-29 09:52:38 +0100

    Problem 12

--- Problem012/Program.cs
+++ Problem012/Program.cs
@@ -1,14 +1,24 @@
-﻿int targetDivisors = 500;
-long currentTriangleNum = 1;
-while(numOfDivisors(currentTriangleNum) <= targetDivisors) currentTriangleNum = currentTriangleNum + currentTriangleNum + 1;
-Console.WriteLine(currentTriangleNum);
+﻿//76576500
+int targetDivisors = 500;
+long currentTriangleNum = 0; 
+
+for(int i = 1; ; i++){
+  currentTriangleNum += i;
+  if(numOfDivisors(currentTriangleNum) >= targetDivisors){
+    Console.WriteLine(currentTriangleNum);
+    return;
+  }
+}
+
 
 int numOfDivisors(long n){
+  double sqrt = Math.Sqrt(n);
   int divisors = 0;
-  int count = 1;
-  while(count < n){
-    if(n % count == 0) divisors++;
-    count++;
+  for(int i = 1; i < sqrt; i++){
+    if(n % i == 0){
+      divisors += 2;
+    }
   }
+  if(sqrt * sqrt == n) divisors++;
   return divisors;
 }
